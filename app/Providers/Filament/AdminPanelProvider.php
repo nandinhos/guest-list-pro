053dfb7b948f4080f96dd1a7733ec34e4cc23f14
Nando Dev <nandinhos@gmail.com>
2026-04-07 04:51:32 -0300

    test: enable only AdminOverview widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\AdminOverview;
+use App\Filament\Widgets\PendingApprovalsWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -42,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                // AdminOverview::class,
+                AdminOverview::class,
                 // PendingApprovalsWidget::class,
             ])
             ->middleware([
