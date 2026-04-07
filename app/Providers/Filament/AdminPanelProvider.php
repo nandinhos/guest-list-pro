4a5afd7fa7a97b49d1dc79df96a82be64233b5e6
Nando Dev <nandinhos@gmail.com>
2026-04-07 04:40:28 -0300

    feat(dashboard): restore AdminOverview and PendingApprovalsWidget metrics

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\AdminOverview;
+use App\Filament\Widgets\PendingApprovalsWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -40,12 +42,10 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Dashboard::class,
             ])
-            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                // AccountWidget::class,
-                // FilamentInfoWidget::class,
-                // AdminOverview::class,
-                // PendingApprovalsWidget::class,
+                AdminOverview::class,
+                PendingApprovalsWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
